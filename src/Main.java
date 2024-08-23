d5588de55874a7bb45586b02e698f9fe8744b7cb
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-23 13:05:44 -0400

    Sadly broken

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,64 @@
 public class Main {
-
-
     public static void main(String[] args) {
         System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
-
         System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
-
         System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
-
         System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
     }
 
-
-    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+    private static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
         if (num1 < 0 && num2 >= 0) {
             return false;
-        }
-
-        if (num1 >= 0 && num2 < 0) {
+        } else if (num1 >= 0 && num2 < 0) {
             return false;
         }
 
-        long num1Check = Math.round(num1);
-        long num2Check = Math.round(num2);
+        double num1Check = num1 >= 0 ? Math.floor(num1) : Math.ceil(num1);
+        double num2Check = num2 >= 0 ? Math.floor(num2) : Math.ceil(num2);
+
         if (num1Check != num2Check) {
             return false;
         }
 
         num1 -= num1Check;
-        num1 *= 10;
         num2 -= num2Check;
+        num1 *= 10;
         num2 *= 10;
 
-        num1Check = Math.round(num1);
-        num2Check = Math.round(num2);
+        num1Check = num1 >= 0 ? Math.floor(num1) : Math.ceil(num1);
+        num2Check = num2 >= 0 ? Math.floor(num2) : Math.ceil(num2);
 
         if (num1Check != num2Check) {
             return false;
         }
 
         num1 -= num1Check;
-        num1 *= 100;
         num2 -= num2Check;
-        num2 *= 100;
+        num1 *= 10;
+        num2 *= 10;
 
-        num1Check = Math.round(num1);
-        num2Check = Math.round(num2);
+        num1Check = num1 >= 0 ? Math.floor(num1) : Math.ceil(num1);
+        num2Check = num2 >= 0 ? Math.floor(num2) : Math.ceil(num2);
 
         if (num1Check != num2Check) {
             return false;
         }
 
         num1 -= num1Check;
-        num1 *= 1000;
         num2 -= num2Check;
-        num2 *= 1000;
+        num1 *= 10;
+        num2 *= 10;
 
-        num1Check = Math.round(num1);
-        num2Check = Math.round(num2);
+        num1Check = num1 >= 0 ? Math.floor(num1) : Math.ceil(num1);
+        num2Check = num2 >= 0 ? Math.floor(num2) : Math.ceil(num2);
 
-        return num1Check == num2Check;
+        if (num1Check != num2Check) {
+            return false;
+        }
+
+        return true;
+
+        //repeat 23 to 35 two more times
+
     }
 }
-
-
-
-
